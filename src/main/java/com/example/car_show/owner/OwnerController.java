7bb9ccb38925973e0285ce8b0c440007a7b1eb53
pottery_stimulation <126481089+swelldisco@users.noreply.github.com>
2024-01-16 14:01:17 -0500

    minor name change to align with classmates

--- src/main/java/com/example/car_show/owner/OwnerController.java
+++ src/main/java/com/example/car_show/owner/OwnerController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/v1/owners")
+@RequestMapping("/api/v1/owner")
 public class OwnerController {
     
     private final OwnerService service;
