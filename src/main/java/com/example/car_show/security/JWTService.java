866fb0933ee886c2905c824b2cfe05fae4eac685
pottery_stimulation <126481089+swelldisco@users.noreply.github.com>
2024-01-18 18:12:23 -0500

    clean up of JWTService and create AccountCredentials.java

---
+++ src/main/java/com/example/car_show/security/JWTService.java
@@ -0,0 +1,64 @@
+package com.example.car_show.security;
+
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Service
+public class JWTService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JWTService.class);
+    private static final long EXPIRATION_TIME = 86400000L;
+    private static final String PREFIX ="Bearer";
+    private static final SecretKey KEY = (SecretKey)generateSecretKey();
+
+    private static Key generateSecretKey() {
+        try{
+            KeyGenerator plzGifKey =  KeyGenerator.getInstance("HmacSHA256");
+            plzGifKey.init(256);
+            return plzGifKey.generateKey();
+        } catch (NoSuchAlgorithmException ex) {
+            LOGGER.warn("Algorithm HmacSHA256 is missing for some reason, and Java KeyGenerator documentation says it should not be.");
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public String getToken(String userName) {
+        String token = Jwts.builder()
+            .subject(userName)
+            .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+            .signWith(KEY)
+            .compact();
+        return token;
+    }
+
+    public String getAuthUser(HttpServletRequest request) {
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (token != null) {
+            String user = Jwts.parser()
+                .verifyWith(KEY)
+                .build()
+                .parseSignedClaims(token.replace(PREFIX, ""))
+                .getPayload()
+                .getSubject();
+            if (user != null) {
+                return user;
+            }
+        } 
+        return null;
+        
+    }
+
+    
+}
