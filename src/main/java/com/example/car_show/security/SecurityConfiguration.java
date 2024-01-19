73b1cf3b31664666cb471c637813927a6304cd1a
pottery_stimulation <126481089+swelldisco@users.noreply.github.com>
2024-01-19 12:39:11 -0500

    removed unused imports

--- src/main/java/com/example/car_show/security/SecurityConfiguration.java
+++ src/main/java/com/example/car_show/security/SecurityConfiguration.java
@@ -10,10 +10,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import org.springframework.security.web.SecurityFilterChain;
